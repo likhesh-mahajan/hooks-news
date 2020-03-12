fda149e2fed488b52d46c8b533de20f94eed75ce
likhesh-m <likhesh@aol.com>
2020-03-12 11:25:35 +1100

    Updates

---
+++ precache-manifest.187579ff891dc0c00c05ac4efadaee3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "c767cb96f82cda9a06e1",
+    "url": "/myapp/static/js/main.c767cb96.chunk.js"
+  },
+  {
+    "revision": "473e77f3b684bb8bf4de",
+    "url": "/myapp/static/js/1.473e77f3.chunk.js"
+  },
+  {
+    "revision": "b6ce1940cf56d775715186de96bf3c80",
+    "url": "/myapp/index.html"
+  }
+];
