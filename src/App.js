cca167cb255f4d7fa0c627522985a5d93903037a
likhesh-m <likhesh@aol.com>
2020-03-19 14:59:03 +1100

    fixed build issues

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
 
   useEffect(() => {
     getResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const getResults = async () => {
